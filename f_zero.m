69bc836de342d368f711efa59b58d56051f32bc0
Alan Lattimer <alattime@vt.edu>
2013-10-01 09:20:57 -0400

    Adding zero function to repository

---
+++ f_zero.m
@@ -0,0 +1,10 @@
+function [ fx,fy,qg ] = f_fcn( x )
+%F_FCN Summary of this function goes here
+%   Detailed explanation goes here
+
+fx = zeros(size(x(:,1)));
+fy = zeros(size(x(:,1)));
+qg = zeros(size(x(:,1)));
+
+end
+
